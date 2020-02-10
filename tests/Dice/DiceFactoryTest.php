3148bc9212e0b63059a3136da6b914864fa4a40c
Hannah Warmbier <hannahwarmbier@gmail.com>
2020-02-09 19:46:55 -0600

    Update dependencies
    Oops Laravel didn’t like that

--- tests/Dice/DiceFactoryTest.php
+++ tests/Dice/DiceFactoryTest.php
@@ -14,7 +14,7 @@ class DiceFactoryTest extends TestCase
     /** @var DiceFactory $factory */
     private $factory;
 
-    public function setUp()
+    public function setUp(): void
     {
         $this->factory = new DiceFactory(new MersenneTwister());
     }
